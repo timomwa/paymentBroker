153c75b4883a8556a87247c6cebca3033d534a92
Timothy Mwangi <timomwa@gmail.com>
2017-02-23 00:08:58 +0300

    Corrected services

--- web/src/main/java/ke/co/technovation/service/InvoiceService.java
+++ web/src/main/java/ke/co/technovation/service/InvoiceService.java
@@ -4,12 +4,13 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import ke.co.technovation.dto.InvoiceValidationRequest;
+import ke.co.technovation.dto.InvoiceValidationResponse;
 
-@WebService(name="invoiceservice" , targetNamespace="http://services.technovation.co.ke")
+@WebService(name="invoiceservice" , targetNamespace="http://services.nda.or.ug")
 public class InvoiceService {
 	
 	@WebMethod
-	public InvoiceService validateInvoice(InvoiceValidationRequest invoiceValidationReq){
+	public InvoiceValidationResponse  validateInvoice(InvoiceValidationRequest invoiceValidationReq){
 		return null;
 	}
 	
